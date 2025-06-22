dc02351997a1c875324f8f98d7b595a7f69e8ed3
Vijendra Aithal <vijendra.aithal@gmail.com>
2025-06-22 20:05:43 +0530

    added api test case for: GET  /brands/{id}
    verified status code 200
    
    verified name of the brand

--- specs/brands.spec.ts
+++ specs/brands.spec.ts
@@ -2,11 +2,19 @@ import * as supertest from 'supertest';
 const req = supertest('https://practice-react.sdetunicorns.com/api/test');
 
 describe('Brands', () => {
-    it.only('GET Brands', async() => {
+    it('GET Brands', async() => {
         const res = await req.get('/brands');
         expect(res.statusCode).toEqual(200);
         expect(res.body.length).toBeGreaterThanOrEqual(1);
         expect(res.body[0]).toHaveProperty('_id');
         expect(res.body[0]).toHaveProperty('name');
     }, 10000);
+
+    it('GET Brand', async() => {
+        const res = await req.get('/brands/6628a70b986188d4dce47f4f');
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.name).toEqual('apple');
+    });
 });
+
+
