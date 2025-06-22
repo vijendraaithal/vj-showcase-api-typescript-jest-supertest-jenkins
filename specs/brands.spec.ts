5c0dfcffb5f7dca425efec6e447848208190551d
Vijendra Aithal <vijendra.aithal@gmail.com>
2025-06-22 20:21:31 +0530

    added api test case for: POST  /brands
    crated a bradn
    
    validated status code to be 200, brand name & property createdAt

--- specs/brands.spec.ts
+++ specs/brands.spec.ts
@@ -1,8 +1,8 @@
 import * as supertest from 'supertest';
 const req = supertest('https://practice-react.sdetunicorns.com/api/test');
 
-describe('Brands', () => {
-    it.only('GET Brands', async() => {
+describe('Fetch Brands', () => {
+    it('GET Brands', async() => {
         const res = await req.get('/brands');
         expect(res.statusCode).toEqual(200);
         expect(res.body.length).toBeGreaterThanOrEqual(1);
@@ -15,5 +15,20 @@ describe('Brands', () => {
         const res = await req.get('/brands/6628a70b986188d4dce47f4f');
         expect(res.statusCode).toEqual(200);
         expect(res.body.name).toEqual('apple');
+    });
+});
+
+describe('Create Brand', () => {
+    
+    it.only('POST Brand', async() => {
+        const data = {
+            name: 'vj-test-brand 22Jun25 Set 01',
+            description: 'Description vj-test-brand 22Jun25 Set 01'
+        }
+        const res = await req.post('/brands')
+            .send(data);
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.name).toEqual(data.name);
+        expect(res.body).toHaveProperty('createdAt');
     });
 });
