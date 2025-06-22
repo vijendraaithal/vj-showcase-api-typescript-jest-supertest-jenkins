d6425e3c0a88c32d278a2c20b57157a2721ed8fe
Vijendra Aithal <vijendra.aithal@gmail.com>
2025-06-22 21:01:06 +0530

    added api test case for: PUT /brands
    created newBrand variable, stored the created brand
    
    used in PUT api via newBody._id
    
    for every run, a brand will get created adn then it will be updated

--- specs/brands.spec.ts
+++ specs/brands.spec.ts
@@ -2,37 +2,57 @@ import * as supertest from 'supertest';
 const req = supertest('https://practice-react.sdetunicorns.com/api/test');
 import {faker} from '@faker-js/faker';
 
-describe('Fetch Brands', () => {
-    it('GET Brands', async() => {
-        const res = await req.get('/brands');
-        expect(res.statusCode).toEqual(200);
-        expect(res.body.length).toBeGreaterThanOrEqual(1);
-        expect(res.body[0]).toHaveProperty('_id');
-        expect(res.body[0]).toHaveProperty('name');
-        expect(Object.keys(res.body[0])).toEqual(['_id', 'name']);
-    }, 10000);
+describe('Brand API Test Suite', () => {
+    let newBrand: any;
+    describe('Fetch Brands', () => {
+        it('GET Brands', async() => {
+            const res = await req.get('/brands');
+            expect(res.statusCode).toEqual(200);
+            expect(res.body.length).toBeGreaterThanOrEqual(1);
+            expect(res.body[0]).toHaveProperty('_id');
+            expect(res.body[0]).toHaveProperty('name');
+            expect(Object.keys(res.body[0])).toEqual(['_id', 'name']);
+        },10000);
 
-    it('GET Brand', async() => {
-        const res = await req.get('/brands/6628a70b986188d4dce47f4f');
-        expect(res.statusCode).toEqual(200);
-        expect(res.body.name).toEqual('apple');
+        it('GET Brand', async() => {
+            const res = await req.get('/brands/6628a70b986188d4dce47f4f');
+            expect(res.statusCode).toEqual(200);
+            expect(res.body.name).toEqual('apple');
+        });
     });
-});
 
-describe('Create Brand', () => {
-    
-    it.only('POST Brand', async() => {
-        const brandName = "BN" 
-            + faker.company.buzzVerb() 
-            + faker.string.alpha({length: {min: 5, max: 7}});
-        const data = {
-            name: brandName,
-            description: "BD" + brandName
-        }
-        const res = await req.post('/brands')
-            .send(data);
-        expect(res.statusCode).toEqual(200);
-        expect(res.body.name).toEqual(data.name);
-        expect(res.body).toHaveProperty('createdAt');
+    describe('Create Brand', () => {
+        it('POST Brand', async() => {
+            const brandName = "BN" 
+                + faker.company.buzzVerb() 
+                + faker.string.alpha({length: {min: 5, max: 7}});
+            const data = {
+                name: brandName,
+                description: "BD" + brandName
+            }
+            const res = await req.post('/brands')
+                .send(data);
+            expect(res.statusCode).toEqual(200);
+            expect(res.body.name).toEqual(data.name);
+            expect(res.body).toHaveProperty('createdAt');
+            newBrand = res.body;
+        });
+    });
+
+    describe('Update Brand', () => {
+        it('PUT Brand', async() => {
+            const brandName = "UPD-BN" 
+                + faker.company.buzzVerb() 
+                + faker.string.alpha({length: {min: 5, max: 7}});
+            const data = {
+                name: brandName,
+                description: "BD" + brandName
+            }
+            const res = await req.put('/brands/' + newBrand._id)
+                .send(data);
+            expect(res.statusCode).toEqual(200);
+            expect(res.body.name).toEqual(data.name);
+            expect(res.body).toHaveProperty('createdAt');
+        });
     });
 });
